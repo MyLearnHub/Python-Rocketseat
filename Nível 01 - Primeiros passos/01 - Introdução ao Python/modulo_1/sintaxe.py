a280204a4bc97d25ffcf49447bb08e732fe2739e
Paulo-Alvares <paulo.alvares@sptech.school>
2026-03-17 18:58:44 -0300

    [doc]: sintax, variables and first program in python

---
+++ Nível 01 - Primeiros passos/01 - Introdução ao Python/modulo_1/sintaxe.py
@@ -0,0 +1,9 @@
+# Esse é meu primeiro comentário
+""" Esse é meu primeiro comentário 
+    Essa é a segunda linha de comentário
+"""
+
+nome = "Gabriel" + " Casemiro"
+
+nome = "Gabriel" + \
+" Casemiro"
